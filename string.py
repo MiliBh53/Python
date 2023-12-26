82a11f6e80b398d97ed8c2cd8bc210953cda785f
MiliBh53 <154458749+MiliBh53@users.noreply.github.com>
2023-12-26 17:13:05 +0530

    Create string.py
    String Examples

---
+++ string.py
@@ -0,0 +1,22 @@
+#String Sample Codes
+#email-sample
+email_Sample = '''
+Hi Mili,
+
+This is the input.
+
+Thank you for your support.
+
+Regards,
+John.
+'''
+print (email_Sample)
+
+course = 'Python for Beginners'
+print (course[1:-1])
+
+##Formatted String --> Prefix your string with an f and use curly bases to dynamically output your string.
+first = 'John'
+last = 'Smith'
+msg = f'{first} [{last}] is a coder'
+print (msg)
